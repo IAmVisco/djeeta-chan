065518254eaae56dd540e597997e464fbd2b7e7a
Alexander <sashok935@gmail.com>
2018-05-03 13:42:36 +0300

    Remove descriptions, comment useless code, add serverinfo and userinfo

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ import asyncio
 import os
 import requests
 import ast
-from datetime import datetime, timedelta
+from datetime import datetime
+from time import strftime
 from pytz import timezone
 from PIL import Image, ImageOps
 import urllib.request
@@ -23,54 +24,54 @@ import urllib.request
 bot = discord.DiscordBot()
 
 # list of insults
-insults_list = (
-	" is a filthy weeb.",
-	", you're like Rapunzel but instead of letting down your hair you let everyone down.",
-	", you got rejected by Aniki.",
-	", is your ass jealous of the amount of shit that just came out of your mouth?",
-	", if I wanted to kill myself I'd climb your ego and jump to your IQ.",
-	", your family tree must be a cactus because everybody on it is a prick",
-	", you're so ugly, when your mom dropped you off at school she got a fine for littering",
-	", it's better to let someone think you are an idiot than to open your mouth and prove it",
-	", your birth certificate is an apology letter from the condom factory",
-	", the only way you'll ever get laid is if you crawl up a chicken's ass and wait",
-	", if laughter is the best medicine, your face must be curing the world",
-	", it looks like your face caught on fire and someone tried to put it out with a hammer",
-	", what language are you speaking? Cause it sounds like bullshit.",
-	", I wasn't born with enough middle fingers to let you know how I feel about you.",
-	", looking at you, I understand why some animals eat their young.",
-	", you are proof that evolution CAN go in reverse.",
-	", if I had a face like yours, I'd sue my parents",
-	", I'm jealous of all the people that haven't met you.",
-	", you're the reason the gene pool needs a lifeguard.",
-	", I would love to insult you...but that would be beyond the level of your intelligence.",
-	", calling you an idiot would be an insult to all stupid people.",
-	", I would ask you how old you are but I know you can't count that high.",
-	", Roses are red violets are blue, God made me pretty, what happened to you?",
-	", You are so ugly when you looked in the mirror your reflection walked away.",
-	", Don't you love nature, despite what it did to you?",
-	", Hell is wallpapered with all your deleted selfies.",
-	", Shock me, say something intelligent.",
-	", Keep talking, someday you'll say something intelligent!",
-	", Brains aren't everything. In your case they're nothing.",
-	", Karma takes too long, I'd rather beat the shit out of you just now.",
-	", Ever since I saw you in your family tree I've wanted to cut it down.",
-	", Normal people live and learn. You just live.",
-	", You are not as bad as people say, you are much, much worse.",
-	", If I had a dollar for every time you said something smart, I'd be broke.",
-	", You want an insult? Right, look at the mirror.",
-	", Just because you have one doesn't mean you have to act like one.",
-	", Aww, it's so cute when you try to talk about things you don't understand.",
-	", Did your parents keep the placenta and throw away the baby?",
-	", You should need a license to be that ugly.",
-	", Are you always this stupid or is today a special occasion?",
-	", If what you don't know can't hurt you, you're invulnerable.",
-)
+# insults_list = (
+# 	" is a filthy weeb.",
+# 	", you're like Rapunzel but instead of letting down your hair you let everyone down.",
+# 	", you got rejected by Aniki.",
+# 	", is your ass jealous of the amount of shit that just came out of your mouth?",
+# 	", if I wanted to kill myself I'd climb your ego and jump to your IQ.",
+# 	", your family tree must be a cactus because everybody on it is a prick",
+# 	", you're so ugly, when your mom dropped you off at school she got a fine for littering",
+# 	", it's better to let someone think you are an idiot than to open your mouth and prove it",
+# 	", your birth certificate is an apology letter from the condom factory",
+# 	", the only way you'll ever get laid is if you crawl up a chicken's ass and wait",
+# 	", if laughter is the best medicine, your face must be curing the world",
+# 	", it looks like your face caught on fire and someone tried to put it out with a hammer",
+# 	", what language are you speaking? Cause it sounds like bullshit.",
+# 	", I wasn't born with enough middle fingers to let you know how I feel about you.",
+# 	", looking at you, I understand why some animals eat their young.",
+# 	", you are proof that evolution CAN go in reverse.",
+# 	", if I had a face like yours, I'd sue my parents",
+# 	", I'm jealous of all the people that haven't met you.",
+# 	", you're the reason the gene pool needs a lifeguard.",
+# 	", I would love to insult you...but that would be beyond the level of your intelligence.",
+# 	", calling you an idiot would be an insult to all stupid people.",
+# 	", I would ask you how old you are but I know you can't count that high.",
+# 	", Roses are red violets are blue, God made me pretty, what happened to you?",
+# 	", You are so ugly when you looked in the mirror your reflection walked away.",
+# 	", Don't you love nature, despite what it did to you?",
+# 	", Hell is wallpapered with all your deleted selfies.",
+# 	", Shock me, say something intelligent.",
+# 	", Keep talking, someday you'll say something intelligent!",
+# 	", Brains aren't everything. In your case they're nothing.",
+# 	", Karma takes too long, I'd rather beat the shit out of you just now.",
+# 	", Ever since I saw you in your family tree I've wanted to cut it down.",
+# 	", Normal people live and learn. You just live.",
+# 	", You are not as bad as people say, you are much, much worse.",
+# 	", If I had a dollar for every time you said something smart, I'd be broke.",
+# 	", You want an insult? Right, look at the mirror.",
+# 	", Just because you have one doesn't mean you have to act like one.",
+# 	", Aww, it's so cute when you try to talk about things you don't understand.",
+# 	", Did your parents keep the placenta and throw away the baby?",
+# 	", You should need a license to be that ugly.",
+# 	", Are you always this stupid or is today a special occasion?",
+# 	", If what you don't know can't hurt you, you're invulnerable.",
+# )
 
 # user id of people to be insulted
-victim_list = (
+# victim_list = (
 
-)
+# )
 
 
 fancy_answer_list = (
@@ -82,10 +83,10 @@ fancy_answer_list = (
 	"Lady luck told me it's "
 )
 
-badWords = (
-	# 'nigger',
-	# 'faggot',
-)
+# badWords = (
+# 	# 'nigger',
+# 	# 'faggot',
+# )
 
 gifDict = {
 	'dayum': 'https://imgur.com/a/pEJEL',
@@ -121,22 +122,12 @@ gifDict = {
 	'nice': 'https://imgur.com/a/mgw6A',
 	'laugh': 'https://imgur.com/a/ps5I5'
 }
-# on review
-# worms https://cdn.discordapp.com/attachments/348218497635188738/418486661249499136/Australien_worms.gif
-# buttblow http://1.media.dorkly.cvcdn.com/17/90/e957f68f0285658f03972f32126cbd8e.gif
-# you diedhttps://cdn.discordapp.com/attachments/348218497635188738/418484586989879296/IcNRfCv.gif
-# spoopy https://cdn.discordapp.com/attachments/348218497635188738/418483087291187200/1517614650820.gif
-# yagami https://imgur.com/gallery/PqVNv54
-# aizen https://cdn.discordapp.com/attachments/348218497635188738/418489907284017172/1307488195874.gif
-# flyer https://cdn.discordapp.com/attachments/348218497635188738/418490600950595585/giant.gif
-# swimsuit https://cdn.discordapp.com/attachments/348218497635188738/418491805349183488/1311802255647.gif
-
-# no name
-# https://78.media.tumblr.com/a0160ed8bb1b955a25f676da6d33da2f/tumblr_ntcha1rO5A1u0tkulo1_500.gif
+# Help me review and add more gifs! https://pastebin.com/v3u8DG22
 
 gw_mode    = False
 gwstart    = datetime(2018, 4, 22, 19, 0, 0, 0, timezone('Asia/Tokyo'))
 prelimsend = datetime(2018, 4, 23, 23, 59, 0, 0, timezone('Asia/Tokyo'))
+startTime  = datetime.now()
 gm         = True
 gn         = True
 casuals_id = '265292778756374529'
@@ -152,12 +143,18 @@ events = [
 ]
 
 for event in events:
-	eventsEmbed.add_field(name=event[0], value=event[1], inline=False)
+	eventsEmbed.add_field(name = event[0], value = event[1], inline = False)
 
 gifEmbed=discord.Embed(title = 'GIF List', description = 'Use ~gif <name> to post a GIF, names are shown below')
 for pair in gifDict.items():
 	gifEmbed.add_field(name = pair[0], value = pair[1], inline = True)
-	gifEmbed.set_footer(text = 'Help me review and add more gifs! https://pastebin.com/v3u8DG22')
+
+def strfdelta(tdelta, fmt):
+    """ timedelta formatter """
+    d = {"D": tdelta.days}
+    d["h"], rem = divmod(tdelta.seconds, 3600)
+    d["m"], d["s"] = divmod(rem, 60)
+    return fmt.format(**d)
 
 #starting up
 @bot.event
@@ -175,7 +172,7 @@ async def on_member_join(member):
 		await asyncio.sleep(5)
 		await bot.add_roles(member, discord.utils.get(member.server.roles, name = "pubs"))
 
-@bot.command(description = 'I will show your desired emote!')
+@bot.command()
 async def emo(emoName:str):
 	"""Shows requested emote.
 
@@ -187,7 +184,7 @@ async def emo(emoName:str):
 	except:
 		await bot.say("No match found.")
 
-@bot.command(description = 'I will show your desired siete emote!')
+@bot.command()
 async def siete(emoName:str):	
 	"""Shows sietefied emote.
 
@@ -199,7 +196,7 @@ async def siete(emoName:str):
 	except:
 		await bot.say("No match found.")
 
-@bot.command(description = 'I will show you a list with all emotes!')
+@bot.command()
 async def emolist():
 	"""Shows all avaible emotes.
 
@@ -208,7 +205,7 @@ async def emolist():
 	"""
 	await bot.say('<https://imgur.com/a/jmGm3>\nHidden cuz big pic')
 
-@bot.command(description = 'I will show you gif emoji!')
+@bot.command()
 async def gif(gifName:str):
 	"""Shows requested GIF emote.
 
@@ -220,7 +217,7 @@ async def gif(gifName:str):
 	except:
 		await bot.say("No match found.")
 
-@bot.command(description = 'I will show you a list of animated emojis!')
+@bot.command()
 async def giflist():
 	"""Shows full list of GIF emotes avaible.
 
@@ -231,7 +228,7 @@ async def giflist():
 	await bot.say(embed = gifEmbed)
 
 #ninja echo
-@bot.command(pass_context = True, description = 'I will say smth. Make me say smth bad and I will ~~stab you~~ add you to visctoms :dagger:')
+@bot.command(pass_context = True, description = 'I will say smth. Make me say smth bad and I will ~~stab you~~ add you to visctoms :dagger:') # description left for memes sake
 async def say(ctx, *, msg:str):
 	"""Echoes message.
 
@@ -243,7 +240,7 @@ async def say(ctx, *, msg:str):
 	await asyncio.sleep(1)
 	await bot.say(msg)
 
-@bot.command(pass_context = True, description = 'I will show you a list of roles that can be (un)assigned.')
+@bot.command(pass_context = True)
 async def roles(ctx):
 	"""Shows roles list.
 
@@ -264,7 +261,7 @@ async def roles(ctx):
 	print(tmp)
 	await bot.say(tmp)
 
-@bot.command(pass_context = True, description = 'I will (un)assign you desired role!')
+@bot.command(pass_context = True)
 async def role(ctx, *, role: discord.Role):
 	"""Assigns or unassigns the role.
 
@@ -282,7 +279,7 @@ async def role(ctx, *, role: discord.Role):
 		except Exception as e:
 			await bot.say("Please check your input again. The format is ~role <role name>. Available roles can be viewed using ~roles.")
 
-@bot.command(description = 'I will roll a dice for you!')
+@bot.command()
 async def roll(roll:str):
 	"""Will roll a dice for you.
 
@@ -304,7 +301,7 @@ async def roll(roll:str):
 		out = "Please check your input again. The format is ~roll <number> or ~roll <NdN>."
 	await bot.say(out)
 
-@bot.command(pass_context = True, description = 'Check if I\'m alive!')
+@bot.command(pass_context = True)
 async def ping(ctx):
 	"""Checks if bot is alive.
 
@@ -314,7 +311,7 @@ async def ping(ctx):
 	await bot.edit_message(msg, "Pong! Time taken: " + str(int((msg.timestamp - 
 		ctx.message.timestamp).microseconds // 1000)) + "ms")
 
-@bot.command(pass_context = True, description = 'I will make a choice for you! The format is ~choose <Option 1>, <Option 2>, etc.')
+@bot.command(pass_context = True)
 async def choose(ctx):
 	"""Makes a choice.
 
@@ -329,7 +326,7 @@ async def choose(ctx):
 	else:
 		await bot.say("Please check your input again. The format is ~choose <Option 1>, <Option 2>, etc.")
 
-@bot.command(description = 'I will show schedule of events from this month!')
+@bot.command()
 async def events():
 	"""Shows upcoming events schedule.
 
@@ -338,7 +335,7 @@ async def events():
 	"""
 	await bot.say(embed = eventsEmbed)
 
-@bot.command(pass_context = True, description = 'Press F to pay respects.')
+@bot.command(pass_context = True)
 async def f(ctx):
 	"""Press F to pay respects.
 
@@ -352,17 +349,17 @@ async def f(ctx):
 		"** has paid their respects for **" + ctx.message.content[3:] + ".**\n", color=0x8b75a5)# + str(count) + " total."
 	await bot.say(embed = respectsMessage)
 
-@bot.command(description = 'I will make a decision for you!')
+@bot.command()
 async def yesno():
 	"""Will send GIF with yes or no answer."""
 	await bot.say(requests.get("http://yesno.wtf/api").json()['image'])
 
-@bot.command(description = 'Random zen quote from GitHub.')
+@bot.command()
 async def zen():
 	"""Sends zen quote from GitHub"""
 	await bot.say(requests.get("https://api.github.com/zen").text)
 
-@bot.command(description = 'I will show when GW round will start/end!')
+@bot.command()
 async def gw():
 	"""Shows GW timings.
 
@@ -409,7 +406,7 @@ async def gw():
 	else:
 		await bot.say('Guild Wars 38 will have **Water** enemies. It is scheduled to arrive in May.')
 
-@bot.command(pass_context = True, description = 'Final is a perv')
+@bot.command(pass_context = True)
 async def disgusting(ctx):
 	"""Shows random disgusted anime girl.
 
@@ -422,7 +419,7 @@ async def disgusting(ctx):
 	elif int(msg[12:]) > 0 and int(msg[12:]) < 39 :
 		await bot.upload(os.getcwd() + '/res/disgusting/' + msg[12:] + '.png')
 
-@bot.command(pass_context = True, description = 'I will show bigger version of your emoji!')
+@bot.command(pass_context = True)
 async def bigmoji(ctx):
 	"""Shows full size of emoji.
 
@@ -442,12 +439,12 @@ async def mai():
 	"""Equivalent to ~emo mai"""
 	await bot.upload(os.getcwd() + '/res/emotes/mai.png')
 
-@bot.command(description = "I will retrieve avatar URL of user you provided!")
+@bot.command()
 async def avatar(user: discord.Member):
 	"""Shows avatar of the user"""
 	await bot.say(user.avatar_url)
 
-@bot.command(description = "I will search gbf.wiki for you!")
+@bot.command()
 async def wiki(*, query: str):
 	"""Searches gbf.wiki"""
 	url = 'https://gbf.wiki/api.php?action=query&list=search&format=json&utf8=&srsearch=' + query
@@ -457,6 +454,46 @@ async def wiki(*, query: str):
 		await bot.say("https://gbf.wiki/" + r['query']['search'][0]["title"].replace(' ', '_'))
 	else:
 		await bot.say("Nothing found, please check your input and try again.")
+
+@bot.command(pass_context = True)
+async def userinfo(ctx, user: discord.Member = None):
+	"""Shows info about user."""
+	if user is None:
+		user = ctx.message.author
+
+	userInfo = discord.Embed(title = user.name + '#' + user.discriminator, color = 0x07f7e2)
+	userInfo.set_thumbnail(url = user.avatar_url)
+	userInfo.add_field(name = 'Status', value = user.status)
+	userInfo.add_field(name = 'ID', value = user.id)
+	userInfo.add_field(name = 'Account creation date', 
+		value = user.created_at.strftime("%d %b %Y %H:%M %Z"))
+	userInfo.add_field(name = 'Server join date', 
+		value = user.joined_at.strftime("%d %b %Y %H:%M %Z"))
+	userInfo.set_footer(text = strfdelta(datetime.now() - startTime, 
+		"Alive for {D} days {h} hours {m} minutes {s} seconds."), 
+		icon_url = bot.user.avatar_url)
+	await bot.say(embed = userInfo)
+
+@bot.command(pass_context = True)
+async def serverinfo(ctx):
+	"""Shows info about server"""
+	server = ctx.message.server;
+	serverInfo = discord.Embed(title = server.name, color = 0x07f7e2)
+	serverInfo.set_thumbnail(url = server.icon_url)
+	serverInfo.add_field(name = 'ID', value = server.id)
+	serverInfo.add_field(name = 'Owner', value = server.owner.name + 
+		'#' + server.owner.discriminator)
+	serverInfo.add_field(name = 'Region', value = server.region)
+	serverInfo.add_field(name = 'Members', value = server.member_count)
+	serverInfo.add_field(name = 'Channels', value = sum(1 for _ in server.channels))
+	serverInfo.add_field(name = 'Creation date', 
+		value = server.created_at.strftime("%d %b %Y %H:%M %Z"))
+	serverInfo.add_field(name = 'Roles list', 
+		value = ', '.join(role.name for role in server.roles))
+	serverInfo.set_footer(text = strfdelta(datetime.now() - startTime, 
+		"Alive for {D} days {h} hours {m} minutes {s} seconds."), 
+		icon_url = bot.user.avatar_url)
+	await bot.say(embed = serverInfo)
 
 @bot.command()
 async def shrug():
@@ -517,15 +554,15 @@ async def on_message(message):
 		elif "\\o/" in message.content.lower():
 			await bot.send_message(message.channel, "\\o/")
 
-	# profanity filter
-	if message.server.id == casuals_id:
-		for word in badWords:
-			if word in message.content.lower():
-				await bot.delete_message(message)
-				await bot.send_message(message.channel, message.author.mention + " is a baka")
-				await bot.add_roles(message.author, discord.utils.get(message.server.roles, name = 'nadeko-mute'))
-				await asyncio.sleep(300)
-				await bot.remove_roles(message.author, discord.utils.get(message.server.roles, name = 'nadeko-mute'))
+	# # profanity filter
+	# if message.server.id == casuals_id:
+	# 	for word in badWords:
+	# 		if word in message.content.lower():
+	# 			await bot.delete_message(message)
+	# 			await bot.send_message(message.channel, message.author.mention + " is a baka")
+	# 			await bot.add_roles(message.author, discord.utils.get(message.server.roles, name = 'nadeko-mute'))
+	# 			await asyncio.sleep(300)
+	# 			await bot.remove_roles(message.author, discord.utils.get(message.server.roles, name = 'nadeko-mute'))
 
 	# double prefix
 	if message.content.startswith('!') and not (message.content.startswith('!emo') 
