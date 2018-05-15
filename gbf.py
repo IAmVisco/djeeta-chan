889a5b87fb2fd395311a39ef8c3c32f32c90de66
Alexander <sashok935@gmail.com>
2018-05-15 22:06:37 +0300

    Split everything into cogs

---
+++ gbf.py
@@ -0,0 +1,98 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+from pytz import timezone
+from utils import RandomColor
+import requests
+
+eventsEmbed=discord.Embed(title="Event schedule", 
+	description="Schedule for May", color = RandomColor())
+events = [
+	["A Thousand Reasons" , "30/04 - 08/05"],
+	["The Girl who Leapt Through Mountains (Rerun)" , "09/05 - 14/05"],
+	["Xeno Vohu Clash (Rerun)" , "15/05 - 22/05"],
+	["Guild Wars (Water Enemies)", "23/05 - 30/05"],
+	["New Scenario Event", "31/05 - 09/06"]
+]
+
+for event in events:
+	eventsEmbed.add_field(name = event[0], value = event[1], inline = False)
+
+gw_mode    = False
+gwstart    = datetime(2018, 4, 22, 19, 0, 0, 0, timezone('Asia/Tokyo'))
+prelimsend = datetime(2018, 4, 23, 23, 59, 0, 0, timezone('Asia/Tokyo'))
+
+class GBF():
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	async def events(self):
+		"""Shows upcoming events schedule.
+
+		If it's outdated, bot host forgot to update it.
+		Poke him/her with a stick, tenderly.
+		"""
+		await self.bot.say(embed = eventsEmbed)
+
+
+	@commands.command()
+	async def gw(self):
+		"""Shows GW timings.
+
+		Very broken on certain days, will fix someday.
+		"""
+		if gw_mode:
+			if (datetime.now(timezone('Asia/Tokyo')).day == gwstart.day and 
+				datetime.now(timezone('Asia/Tokyo')).hour >= 19) or \
+				datetime.now(timezone('Asia/Tokyo')).day == gwstart.day + 1:
+				await self.bot.say(':point_right: :clock12: | Prelims end in '+ str(prelimsend.day -
+					datetime.now(timezone('Asia/Tokyo')).day) + ' days ' + str(prelimsend.hour -
+					datetime.now(timezone('Asia/Tokyo')).hour) + ' hours ' + str(prelimsend.minute -
+					datetime.now(timezone('Asia/Tokyo')).minute) + ' minutes.')	
+			# elif (datetime.now(timezone('Asia/Tokyo')).day <= gwstart.day):
+			# 	if (gwstart.hour - datetime.now(timezone('Asia/Tokyo')).hour >= 0):
+			# 		await self.bot.say(':point_right: :clock1: | Prelims start in '+ str(gwstart.day - 
+			# 			datetime.now(timezone('Asia/Tokyo')).day) + ' days ' + str(gwstart.hour - 
+			# 			datetime.now(timezone('Asia/Tokyo')).hour) + ' hours ' + str(60 - 
+			# 			datetime.now(timezone('Asia/Tokyo')).minute) + ' minutes.')
+			# 	else:
+			# 		await self.bot.say(':point_right: :clock1: | Prelims start in ' + str(24 - 
+			# 			(gwstart.hour - datetime.now(timezone('Asia/Tokyo')).hour)) + ' hours ' + str(60 - 
+			# 			datetime.now(timezone('Asia/Tokyo')).minute) + ' minutes.')
+			elif (datetime.now(timezone('Asia/Tokyo')).hour >= 7):
+				if 23 - datetime.now(timezone('Asia/Tokyo')).hour != 0:
+					await self.bot.say(':point_right: :clock12: | Round ' + 
+						str(datetime.now(timezone('Asia/Tokyo')).day - 24) +
+						' ends in ' + str(23 - datetime.now(timezone('Asia/Tokyo')).hour) + ' hours ' +
+						str(60 - datetime.now(timezone('Asia/Tokyo')).minute) + ' minutes.')
+				else:
+					await self.bot.say(':point_right: :clock12: | Round ' + str(datetime.now(timezone('Asia/Tokyo')).day - 24) + 
+						' ends in ' + str(60 - datetime.now(timezone('Asia/Tokyo')).minute) + ' minutes.')
+
+			elif datetime.now(timezone('Asia/Tokyo')).day - 24 <= 5: # day of start(14) + 2
+				if 6 - datetime.now(timezone('Asia/Tokyo')).hour != 0:
+					await self.bot.say(':point_right: :clock7: | Round ' + str(datetime.now(timezone('Asia/Tokyo')).day - 24) + 
+						' starts in ' + str(6 - datetime.now(timezone('Asia/Tokyo')).hour) + ' hours ' + 
+						str(60 - datetime.now(timezone('Asia/Tokyo')).minute) + ' minutes.')
+				else:
+					await self.bot.say(':point_right: :clock7: | Round ' + str(datetime.now(timezone('Asia/Tokyo')).day - 24) + 
+						' starts in ' + str(60 - datetime.now(timezone('Asia/Tokyo')).minute) + ' minutes.')
+			else:
+				await self.bot.say('Guild Wars 37 is over, thanks for your hard work.')
+		else:
+			await self.bot.say('Guild Wars 38 will have **Water** enemies. It is scheduled to arrive in May.')
+
+	@commands.command()
+	async def wiki(self,*, query: str):
+		"""Searches gbf.wiki"""
+		url = 'https://gbf.wiki/api.php?action=query&list=search&format=json&utf8=&srsearch=' + query
+		r = requests.get(url = url).json()
+		print(query)
+		if r["query"]["searchinfo"]["totalhits"] != 0:
+			await self.bot.say("https://gbf.wiki/" + r['query']['search'][0]["title"].replace(' ', '_'))
+		else:
+			await self.bot.say("Nothing found, please check your input and try again.")
+
+def setup(bot):
+	bot.add_cog(GBF(bot))
