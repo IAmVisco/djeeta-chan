ff6c0f09e228c13c921b0e6f6536099df9dcdc21
Alexander <sashok935@gmail.com>
2018-11-04 22:10:30 +0300

    Bump ver

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ from random import randint
 # from pathlib import Path
 
 bot = discord.DiscordBot()
+# reading db conn string, should be done from os.env but I am lazy
 config = config.Config('settings.json', directory="")
 conn = psycopg2.connect(config.get("DB_URL", ""), sslmode='require')
 db = conn.cursor()
