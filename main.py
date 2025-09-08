20c83d3fe4c873012a0d782cf1c7da3911294f80
Alexander <sashok935@gmail.com>
2025-09-08 19:38:06 +0200

    Add marvelous, add some types

--- main.py
+++ main.py
@@ -1,62 +1,34 @@
 import asyncio
 import os
-import re
 import sys
 import traceback
+from typing import Any
 
 import discord
 import logging
-import random as r
 from discord.ext import commands
+from utils import process_message_replies, handle_keyword_reaction
 
-__version__ = '3.1.1'
-intents = discord.Intents.all()
+__version__: str = '3.2.0'
+intents: discord.Intents = discord.Intents.all()
 logging.basicConfig(format="%(levelname)s:%(asctime)s:%(message)s", level=logging.INFO, stream=sys.stdout)
-bot = commands.Bot(command_prefix="~",
+bot: commands.Bot = commands.Bot(command_prefix="~",
                    description="Djeeta bot! Has some cool commands and a bunch of emotes.",
                    intents=intents
                    )
 
 
 @bot.event
-async def on_ready():
+async def on_ready() -> None:
     await bot.change_presence(activity=discord.Game(name="~help to help"))
     logging.info(" {0.user.name} bot {1} - Ready. Running on Python v{2.major}.{2.minor}.{2.micro}-{2.releaselevel} "
                  .format(bot, __version__, sys.version_info).center(70, "="))
 
 
-def process_message_replies(message):
-    # TODO: Move this to better place
-    out = None
-    if "/o/" in message.content.lower():
-        out = "\\o\\"
-    elif "\\o\\" in message.content.lower():
-        out = "/o/"
-    elif re.search(r"(^|\W)(ay{2,})($|\W)", message.content.lower()) is not None:
-        out = "LMAO" if "AYY" in message.content else "lmao"
-    elif "\\o/" in message.content.lower():
-        out = "\\o/"
-    elif re.search(r"(^|\W)(owo)($|\W)", message.content.lower()) is not None:
-        if r.randint(1, 100) <= 20:
-            out = r.choice(["kys", "uwu", "o3o", " *nuzzles wuzzles*", "no", "Rawr xD", "·///·"])
-        else:
-            out = "What's this?"
-
-    return out
-
-
 @bot.event
 @commands.guild_only()
-async def on_message(message):
-    keyword = None
-    if "GoodMorning" in message.content:
-        keyword = "GoodMorning"
-    elif "GoodNight" in message.content:
-        keyword = "GoodNight"
-    if keyword:
-        emoji = discord.utils.find(lambda e: e.name == keyword, message.guild.emojis)
-        if emoji:
-            await message.add_reaction(emoji)
+async def on_message(message: discord.Message) -> None:
+    await handle_keyword_reaction(message)
 
     out = process_message_replies(message) if not message.author.bot else None
 
@@ -71,7 +43,7 @@ async def on_message(message):
 
 
 @bot.event
-async def on_command_completion(ctx):
+async def on_command_completion(ctx: commands.Context) -> None:
     if isinstance(ctx.channel, discord.DMChannel):
         template = "{0.author} invoked '{0.message.content}' in {0.channel}"
     else:
@@ -80,7 +52,7 @@ async def on_command_completion(ctx):
 
 
 @bot.event
-async def on_command_error(ctx, exception):
+async def on_command_error(ctx: commands.Context, exception: Exception) -> None:
     if hasattr(ctx.command, 'on_error'):
         return
 
@@ -100,7 +72,7 @@ async def on_command_error(ctx, exception):
     traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
 
 
-async def main():
+async def main() -> None:
     for file in os.listdir("cogs"):
         if file.endswith(".py"):
             name = file[:-3]
--- utils.py
+++ utils.py
@@ -1,22 +1,78 @@
 import os
+from typing import Any, Optional
 import pytz
 import aiohttp
 import asyncpg
 import random as r
+import re
+import discord
+import datetime as dt
 
 
-def random_color():
+sunday_image_url: str = "https://fixvx.com/notnoe_/status/1964794280469540951"
+weekday_image_url: str = "https://tenor.com/view/uma-uma-musume-musume-marvelous-sunday-marvelous-gif-15092128582077845004"
+
+def random_color() -> int:
     return int('0x' + ''.join([r.choice('0123456789ABCDEF') for _ in range(6)]), 0)
 
 
-def get_utc_string(dt):
-    return dt.astimezone(pytz.utc).strftime("%d %b %Y %H:%M %Z")
+def get_utc_string(datetime: dt.datetime) -> str:
+    return datetime.astimezone(pytz.utc).strftime("%d %b %Y %H:%M %Z")
 
-async def connect_to_db():
-    return await asyncpg.connect(dsn=os.environ.get("DATABASE_URL"))
+async def connect_to_db() -> asyncpg.Connection:
+    dsn = os.environ.get("DATABASE_URL")
+    return await asyncpg.connect(dsn=dsn)
 
 
-async def get_json_data(url, content_type='application/json'):
+async def get_json_data(url: str, content_type: str = 'application/json') -> Any:
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             return await resp.json(content_type=content_type)
+
+def process_message_replies(message: discord.Message) -> Optional[str]:
+    out: Optional[str] = None
+    content_lower = message.content.lower()
+    if "/o/" in content_lower:
+        out = "\\o\\"
+    elif "\\o\\" in content_lower:
+        out = "/o/"
+    elif re.search(r"(^|\W)(ay{2,})($|\W)", content_lower) is not None:
+        out = "LMAO" if "AYY" in message.content else "lmao"
+    elif "\\o/" in content_lower:
+        out = "\\o/"
+    elif re.search(r"(^|\W)(owo)($|\W)", content_lower) is not None:
+        if r.randint(1, 100) <= 20:
+            out = r.choice(["kys", "uwu", "o3o", " *nuzzles wuzzles*", "no", "Rawr xD", "·///·"])
+        else:
+            out = "What's this?"
+    return out
+
+async def send_marvelous_image(message: discord.Message) -> None:
+    today = dt.datetime.now(dt.UTC).weekday()
+    is_sunday = today == 6
+
+    image_url = sunday_image_url if is_sunday else weekday_image_url
+
+    await message.channel.send(image_url)
+
+async def handle_keyword_reaction(message: discord.Message) -> None:
+    if not isinstance(message.channel, (discord.TextChannel, discord.Thread, discord.ForumChannel)):
+        # For DMs or non-guild channels, skip emoji search that requires guild
+        return
+
+    content = message.content
+
+    if "marvelous" in content.lower():
+        await send_marvelous_image(message)
+
+    keyword = None
+    if "GoodMorning" in content:
+        keyword = "GoodMorning"
+    elif "GoodNight" in content:
+        keyword = "GoodNight"
+    if not keyword:
+        return
+
+    emoji = discord.utils.find(lambda e: e.name == keyword, message.guild.emojis)
+    if emoji:
+        await message.add_reaction(emoji)
